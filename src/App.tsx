2ce2aa82e7b6ee91e648dd5ccffc836ec89b4ec6
Soukkio <petri.soukkio@live.com>
2021-12-31 16:31:07 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Moro</h1>
+      <h2>Just testing from sandbox to directly to github</h2>
+      <h3>BTW Moro is the Tampere (city in Finland) way to say hello</h3>
+    </div>
+  );
+}
